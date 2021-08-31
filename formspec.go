cddebf009191cd7c2ac808f767bb9de30278977e
HimbeerserverDE <himbeerserverde@gmail.com>
2021-08-31 11:00:31 +0200

    Fix formspec server name prepender (#19)

--- formspec.go
+++ formspec.go
@@ -7,7 +7,7 @@ import (
 
 func (sc *serverConn) prependFormspec(fs *string) {
 	reg := regexp.MustCompile("[^a-zA-Z0-9-_.:]")
-	reg2 := regexp.MustCompile("[a-zA-Z0-9-_.]*\\.[a-zA-Z0-9-_.]+")
+	reg2 := regexp.MustCompile("[a-zA-Z0-9-_.]*\\.[a-zA-Z-_.]+")
 	subs := reg.Split(*fs, -1)
 	seps := reg.FindAllString(*fs, -1)
 
