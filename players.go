4c5063b9f9c54254fd9b17adcda3279f0cf7aaaa
HimbeerserverDE <himbeerserverde@gmail.com>
2021-09-09 16:40:31 +0200

    Expose player name list to plugins

--- players.go
+++ players.go
@@ -4,3 +4,15 @@ import "sync"
 
 var players = make(map[string]struct{})
 var playersMu sync.RWMutex
+
+func Players() map[string]struct{} {
+	playersMu.RLock()
+	defer playersMu.RUnlock()
+
+	p := make(map[string]struct{})
+	for player := range players {
+		p[player] = struct{}{}
+	}
+
+	return p
+}
