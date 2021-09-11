83d208ed951018e5dbefe2d1e4d80dbedbab3cfb
HimbeerserverDE <himbeerserverde@gmail.com>
2021-09-11 10:18:53 +0200

    Revert "Prevent empty disconnect reasons"
    This reverts commit ccec89bce62cbcb1c067b51509d11a8bee43f0f2.

--- moderation.go
+++ moderation.go
@@ -5,10 +5,6 @@ import "github.com/anon55555/mt"
 // Kick sends mt.ToCltDisco with the specified custom reason
 // and closes the ClientConn.
 func (cc *ClientConn) Kick(reason string) {
-	if reason == "" {
-		reason = "Kicked by proxy."
-	}
-
 	ack, _ := cc.SendCmd(&mt.ToCltDisco{
 		Reason: mt.Custom,
 		Custom: reason,
