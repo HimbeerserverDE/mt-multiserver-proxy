f46ce4b0028ccc06482064bdf5c392a198989853
HimbeerserverDE <himbeerserverde@gmail.com>
2021-09-11 10:25:59 +0200

    Ensure Kick is run in a new goroutine

--- moderation.go
+++ moderation.go
@@ -5,14 +5,16 @@ import "github.com/anon55555/mt"
 // Kick sends mt.ToCltDisco with the specified custom reason
 // and closes the ClientConn.
 func (cc *ClientConn) Kick(reason string) {
-	ack, _ := cc.SendCmd(&mt.ToCltDisco{
-		Reason: mt.Custom,
-		Custom: reason,
-	})
+	go func() {
+		ack, _ := cc.SendCmd(&mt.ToCltDisco{
+			Reason: mt.Custom,
+			Custom: reason,
+		})
 
-	select {
-	case <-cc.Closed():
-	case <-ack:
-		cc.Close()
-	}
+		select {
+		case <-cc.Closed():
+		case <-ack:
+			cc.Close()
+		}
+	}()
 }
