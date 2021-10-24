813d03a5c700947973d3a5e90b4f0242f3b5a896
HimbeerserverDE <himbeerserverde@gmail.com>
2021-10-24 15:02:07 +0200

    Document Path function

--- proxy.go
+++ proxy.go
@@ -26,6 +26,8 @@ var playerNameChars = regexp.MustCompile("^[a-zA-Z0-9-_]+$")
 var proxyDir string
 var proxyDirOnce sync.Once
 
+// Path prepends the directory the executable is in to the given path.
+// It does not follow symlinks.
 func Path(path ...string) string {
 	proxyDirOnce.Do(func() {
 		executable, err := os.Executable()
