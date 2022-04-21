2018321cc3de4f562f033a17c03f1088f1c1064e
HimbeerserverDE <himbeerserverde@gmail.com>
2022-04-21 14:50:11 +0200

    Fix base64 padding

--- content.go
+++ content.go
@@ -233,8 +233,8 @@ func handleContent(cc *contentConn) {
 
 		RequestLoop:
 			for i, f := range cmd.Files {
-				if len(f.Base64SHA1)%2 != 0 {
-					cmd.Files[i].Base64SHA1 += "="
+				if pad := len(f.Base64SHA1) % 4; pad != 0 {
+					cmd.Files[i].Base64SHA1 += strings.Repeat("=", pad)
 				}
 
 				if cc.fromCache(f.Name, f.Base64SHA1) {
