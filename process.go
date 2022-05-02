cd1ccf7001e72dec2bf9cc4bcc8960b4d28cda6c
HimbeerserverDE <himbeerserverde@gmail.com>
2022-05-02 19:33:19 +0200

    Remove newline

--- process.go
+++ process.go
@@ -445,7 +445,6 @@ func (cc *ClientConn) process(pkt mt.Pkt) {
 		done := make(chan struct{})
 
 		go func(done chan<- struct{}) {
-
 			result, isCmd := onChatMsg(cc, cmd)
 			if !isCmd {
 				forward(pkt)
