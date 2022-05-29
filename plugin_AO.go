2b86b9308a978ebae483f8777480c9efb07d78dd
riley <riley@e926.de>
2022-05-29 20:33:26 +0200

    GetAOID & FreeAOID

--- plugin_AO.go
+++ plugin_AO.go
@@ -77,3 +77,175 @@ func RegisterAOHandler(h *AOHandler) {
 
 	aOHandlers = append(aOHandlers, h)
 }
+
+///
+/// - AOID managment
+///
+
+var LowestAOID mt.AOID = 0xA000
+
+///
+/// - Global AOIDs
+///
+
+type globalAOID struct {
+	used, client, server, global bool
+}
+
+var globalAOIDs = make(map[mt.AOID]globalAOID)
+var globalAOIDsMu sync.RWMutex
+
+// GetGlobalAOID returns a globally unused AOID
+func GetGlobalAOID() (bool, mt.AOID) {
+	globalAOIDsMu.Lock()
+	defer globalAOIDsMu.Unlock()
+
+	notFound := true
+	var id mt.AOID = 0xFFFF
+	for notFound {
+		if globalAOIDs[id].used == false {
+			notFound = false
+
+			globalAOIDs[id] = globalAOID{used: true, global: true}
+		} else if id < LowestAOID {
+			return false, 0xFFFF
+		} else {
+			id--
+		}
+	}
+
+	return !notFound, id
+}
+
+// FreeAOID marks a id as globally unused
+func FreeGlobalAOID(id mt.AOID, srv string) {
+	globalAOIDsMu.Lock()
+	defer globalAOIDsMu.Unlock()
+
+	globalAOIDs[id] = globalAOID{used: false}
+}
+
+///
+/// - Server AOIDs
+///
+
+type sAOID map[mt.AOID]bool
+
+var serverAOIDs = make(map[string]sAOID)
+var serverAOIDsMu sync.RWMutex
+
+func (sm sAOID) empty() bool {
+	for _, v := range sm {
+		if v {
+			return false
+		}
+	}
+
+	return true
+}
+
+// GetServerAOId returns a free mt.AOID for a server, which will be marked as used
+func GetServerAOId(srv string) (bool, mt.AOID) {
+	globalAOIDsMu.Lock()
+	defer globalAOIDsMu.Unlock()
+
+	serverAOIDsMu.Lock()
+	defer serverAOIDsMu.Unlock()
+
+	notFound := true
+	var id mt.AOID = 0xFFFF
+	for notFound {
+		if globalAOIDs[id].used == false || (globalAOIDs[id].server == true && !serverAOIDs[srv][id]) {
+			notFound = false
+			if serverAOIDs[srv] == nil {
+				serverAOIDs[srv] = make(map[mt.AOID]bool)
+			}
+			serverAOIDs[srv][id] = true
+			globalAOIDs[id] = globalAOID{used: true, server: true}
+		} else if id < LowestAOID {
+			return false, 0xFFFF
+		} else {
+			id--
+		}
+	}
+
+	return !notFound, id
+}
+
+// FreeServerAOID frees a server AOID
+func FreeServerAOID(srv string, id mt.AOID) {
+	globalAOIDsMu.Lock()
+	defer globalAOIDsMu.Unlock()
+
+	serverAOIDsMu.Lock()
+	defer serverAOIDsMu.Unlock()
+
+	serverAOIDs[srv][id] = false
+	if serverAOIDs[srv].empty() {
+		globalAOIDs[id] = globalAOID{used: false}
+	}
+}
+
+///
+/// - clientbound AOIDs
+///
+
+type cAOID map[mt.AOID]bool
+
+var clientAOIDs = make(map[string]cAOID)
+var clientAOIDsMu sync.RWMutex
+
+func (ca cAOID) empty() bool {
+	for _, v := range ca {
+		if v {
+			return false
+		}
+	}
+
+	return true
+}
+
+// GetFreeAOID returns the next free AOID for a client
+func (cc *ClientConn) GetFreeAOID() (bool, mt.AOID) {
+	globalAOIDsMu.Lock()
+	defer globalAOIDsMu.Unlock()
+
+	clientAOIDsMu.Lock()
+	defer clientAOIDsMu.Unlock()
+
+	name := cc.Name()
+
+	notFound := true
+	var id mt.AOID = 0xFFFF
+	for notFound {
+		if globalAOIDs[id].used == false || (globalAOIDs[id].client == true && !clientAOIDs[name][id]) {
+			notFound = false
+			if clientAOIDs[name] == nil {
+				clientAOIDs[name] = make(map[mt.AOID]bool)
+			}
+			clientAOIDs[name][id] = true
+			globalAOIDs[id] = globalAOID{used: true, client: true}
+		} else if id < LowestAOID {
+			return false, 0xFFFF
+		} else {
+			id--
+		}
+	}
+
+	return !notFound, id
+}
+
+// FreeAOID marks AOID as free
+func (cc *ClientConn) FreeAOID(id mt.AOID) {
+	globalAOIDsMu.Lock()
+	defer globalAOIDsMu.Unlock()
+
+	clientAOIDsMu.Lock()
+	defer clientAOIDsMu.Unlock()
+
+	name := cc.Name()
+	clientAOIDs[name][id] = false
+	if clientAOIDs[name].empty() {
+		globalAOIDs[id] = globalAOID{used: false}
+	}
+}
