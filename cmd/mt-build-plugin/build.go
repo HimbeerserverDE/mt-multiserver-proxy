538bba477414e90e8d164903997cbfd8a90682dd
HimbeerserverDE <himbeerserverde@gmail.com>
2023-11-26 16:25:27 +0100

    remove unused variables and functions

--- cmd/mt-build-plugin/build.go
+++ cmd/mt-build-plugin/build.go
@@ -9,29 +9,12 @@ package main
 
 import (
 	"log"
-	"os"
-	"os/exec"
 
 	proxy "github.com/HimbeerserverDE/mt-multiserver-proxy"
 )
 
 func main() {
-	version, ok := proxy.Version()
-	if !ok {
-		log.Fatal("unable to retrieve proxy version")
-	}
-
 	if err := proxy.BuildPlugin(); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func goCmd(args ...string) error {
-	cmd := exec.Command("go", args...)
-
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	return cmd.Run()
 }
