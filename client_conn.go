530ae608a0df5f9f961bd4bbc3e5eda9ddeb3bc0
HimbeerserverDE <himbeerserverde@gmail.com>
2023-12-06 19:33:01 +0100

    fix ClientConn.hasPlayerCAO logic

--- client_conn.go
+++ client_conn.go
@@ -67,7 +67,7 @@ type ClientConn struct {
 // Name returns the player name of the ClientConn.
 func (cc *ClientConn) Name() string { return cc.name }
 
-func (cc *ClientConn) hasPlayerCAO() bool { return cc.playerCAO == 0 }
+func (cc *ClientConn) hasPlayerCAO() bool { return cc.playerCAO != 0 }
 
 func (cc *ClientConn) server() *ServerConn {
 	cc.mu.RLock()
