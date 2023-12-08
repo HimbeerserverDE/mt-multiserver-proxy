0c196150d876c20ebb67ba29fe2113c87352e73a
HimbeerserverDE <himbeerserverde@gmail.com>
2023-12-08 23:03:00 +0100

    fix hopping documentation: client isn't kicked in an all-dead scenario

--- hop.go
+++ hop.go
@@ -17,7 +17,8 @@ var (
 // Hop connects the ClientConn to the specified upstream server
 // or the first working fallback server, saving the player's last server
 // unless `ForceDefaultSrv` is enabled.
-// If all attempts fail the client is kicked.
+// If all attempts fail the client stays connected to the current server
+// with the potential for inconsistent state.
 // At the moment the ClientConn is NOT fixed if an error occurs
 // so the player may have to reconnect.
 func (cc *ClientConn) Hop(serverName string) (err error) {
@@ -55,7 +56,8 @@ func (cc *ClientConn) Hop(serverName string) (err error) {
 // unless `ForceDefaultSrv` is enabled.
 // See the documentation on `Server.Groups` in `doc/config.md`
 // for details on how a specific game server is selected from the group name.
-// If all attempts fail the client is kicked.
+// If all attempts fail the client stays connected to the current server
+// with the potential for inconsistent state.
 // At the moment the ClientConn is NOT fixed if an error occurs
 // so the player may have to reconnect.
 func (cc *ClientConn) HopGroup(groupName string) error {
