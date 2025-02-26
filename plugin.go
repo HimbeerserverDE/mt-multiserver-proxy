8d3572a53e9080024a21f5493e7ca180cddbebd9
Himbeer <himbeer@disroot.org>
2025-02-26 09:10:23 +0100

    Update check for development build to new Go pseudo-version format

--- plugin.go
+++ plugin.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/exec"
 	"plugin"
+	"strings"
 	"sync"
 )
 
@@ -16,7 +17,7 @@ func BuildPlugin() error {
 		return err
 	}
 
-	if version == "(devel)" {
+	if strings.Contains(version, "+dirty") {
 		return buildPluginDev(version)
 	}
 
